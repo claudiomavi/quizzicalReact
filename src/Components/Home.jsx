5fffa607af6b2b92f514117a8ff489accda2a531
Claudio <claudio.marino@outlook.com>
2024-07-22 20:28:23 +0200

    ok

---
+++ src/Components/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div className="home">
+			<h3 className="home--title">Quizzical</h3>
+			<p className="home--description">Some description if needed</p>
+			<button className="home--button">Start quiz</button>
+		</div>
+	);
+}
