77b12e3b0865e16ed7e19d67394c606fc59c01f8
Claudio Marino <claudio.marino@outlook.com>
2024-07-23 16:14:55 +0200

    started the Questions component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-// CREAR EL COMPONENTE DE HOME Y ESTILARLO
-
 // CREAR EL COMPONENTE DE PREGUNTAS Y ESTILARLO
 // ESTILAR LAS PREGUNTAS COMO RADIO BUTTONS (VER EN GOOGLE COMO ESTILAR EL RADIO COMO UN BUTTON)
 
 // HACER QUE CUANDO SE PULSE EL BOTÓN EN HOME CON UN STATE APAREZCA EL COMPONENTE DE PREGUNTAS
 
-import Home from "/src/Components/Home";
+// import Home from "/src/Components/Home";
+import Questions from "/src/Components/Questions";
 
 export default function App() {
 	return (
 		<div>
-			<Home />
+			{/* <Home /> */}
+			<Questions />
 		</div>
 	);
 }
---
+++ src/Components/Questions.jsx
@@ -0,0 +1,13 @@
+export default function Questions() {
+	return (
+		<div className="questions">
+			<p className="questions--question">Some description if needed</p>
+			<input type="radio" name="answers-1" />
+			<input type="radio" name="answers-1" />
+			<input type="radio" name="answers-1" />
+			<input type="radio" name="answers-1" />
+			<input type="radio" name="answers-1" />
+			<button className="question--button">Check answers</button>
+		</div>
+	);
+}
