f8d49350e14a6032c51ed47aceb177b87b03158a
Claudio <claudio.marino@outlook.com>
2024-07-23 18:31:14 +0200

    styled all the questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-// CREAR EL COMPONENTE DE PREGUNTAS Y ESTILARLO
-// ESTILAR LAS PREGUNTAS COMO BOTONES
 // HACER QUE CUANDO SE PULSE EL BOTÓN EN HOME CON UN STATE APAREZCA EL COMPONENTE DE PREGUNTAS
 
-// import Home from "/src/Components/Home";
+import Home from "/src/Components/Home";
 import Questions from "/src/Components/Questions";
 
 export default function App() {
-	return (
-		<div>
-			{/* <Home /> */}
-			<Questions />
-		</div>
-	);
+	const showQuestions = true;
+
+	return <div>{showQuestions ? <Home /> : <Questions />}</div>;
 }
--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -1,16 +1,67 @@
 export default function Questions() {
 	return (
 		<div className="question">
-			<p className="question--question">
-				How would one say goodbye in Spanish?
-			</p>
 			<div>
-				<button>Adiós</button>
-				<button>Hola</button>
-				<button>Au Revoir</button>
-				<button>Salir</button>
+				<p className="question--question">
+					How would one say goodbye in Spanish?
+				</p>
+				<div>
+					<button className="question--button">Adiós</button>
+					<button className="question--button">Hola</button>
+					<button className="question--button">Au Revoir</button>
+					<button className="question--button">Salir</button>
+				</div>
+				<hr />
 			</div>
-			<hr />
+			<div>
+				<p className="question--question">
+					Which best selling toy of 1983 caused hysteria, resulting in riots
+					breaking in stores?
+				</p>
+				<div>
+					<button className="question--button">Adiós</button>
+					<button className="question--button">Hola</button>
+					<button className="question--button">Au Revoir</button>
+					<button className="question--button">Salir</button>
+				</div>
+				<hr />
+			</div>
+			<div>
+				<p className="question--question">
+					What is the hottest planet in our Solar System?
+				</p>
+				<div>
+					<button className="question--button">Adiós</button>
+					<button className="question--button">Hola</button>
+					<button className="question--button">Au Revoir</button>
+					<button className="question--button">Salir</button>
+				</div>
+				<hr />
+			</div>
+			<div>
+				<p className="question--question">
+					In which country was the caesar salad invented?
+				</p>
+				<div>
+					<button className="question--button">Adiós</button>
+					<button className="question--button">Hola</button>
+					<button className="question--button">Au Revoir</button>
+					<button className="question--button">Salir</button>
+				</div>
+				<hr />
+			</div>
+			<div>
+				<p className="question--question">
+					How Many Hearts Does An Octopus Have?
+				</p>
+				<div>
+					<button className="question--button">Adiós</button>
+					<button className="question--button">Hola</button>
+					<button className="question--button">Au Revoir</button>
+					<button className="question--button">Salir</button>
+				</div>
+				<hr />
+			</div>
 			<button className="question--button">Check answers</button>
 		</div>
 	);
