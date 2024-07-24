c9c0893f513f388bb7828806e93b9eb02da9f31d
Claudio <claudio.marino@outlook.com>
2024-07-24 21:35:02 +0200

    fix an error

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Home from "/src/Components/Home";
 import Questions from "/src/Components/Questions";
 
 export default function App() {
-	const [showQuestions, setShowQuestions] = React.useState(false);
+	const [showQuestions, setShowQuestions] = React.useState(true); // volver a ponerlo en false cuando acabes para que salga la home
 
 	function toggleButton() {
-		return setShowQuestions(false); // volver a ponerlo en true cuando acabes para que salga la home
+		return setShowQuestions(true);
 	}
 
 	return (
